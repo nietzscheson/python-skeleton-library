de85edb61a47a529253362ab2102badfb3f31395
Cristian Angulo Nova <cristianangulonova@gmail.com>
2021-08-11 23:14:51 -0500

    feat: Add Coverage support. Add black formatter

--- tests/test_skeleton.py
+++ tests/test_skeleton.py
@@ -3,7 +3,6 @@ from src.skeleton import Skeleton
 
 
 class SkeletonTestCase(unittest.TestCase):
-
     def test_message(self):
         message = "The Skeleton Message"
         skeleton = Skeleton()
@@ -11,5 +10,5 @@ class SkeletonTestCase(unittest.TestCase):
         self.assertAlmostEqual(skeleton.getMessage(), message)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
